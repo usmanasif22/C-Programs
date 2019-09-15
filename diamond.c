c8f0cfbe4497457b6d2a480857965e340da43a55
Usman Butt <55312364+usmanasif22@users.noreply.github.com>
2019-09-15 21:25:12 +0500

    Few Basic codes of C
    How to print Diamond and Square Shape in C language

---
+++ diamond.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    //Printing Diamond Using printf
+    printf("  *\n");
+    printf(" ***\n");
+    printf("*****\n");
+    printf(" ***\n");
+    printf("  *\n");
+    return 0;
+}
---
+++ square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    //Printing Square Using printf
+    printf("**********\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("**********");
+    return 0;
+}
